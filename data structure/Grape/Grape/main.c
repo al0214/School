c01557c2e3fbbcd46eb6dfc65853f89c4681e9d4
al0214 <sisang951@hansei.sen.hs.kr>
2023-04-14 14:27:29 +0900

    CSS

---
+++ data structure/Grape/Grape/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Grape
+//
+//  Created by SamoYed on 2023/04/12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
