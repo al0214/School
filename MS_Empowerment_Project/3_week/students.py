53cf54ac031a481274e41ff7e9a471a566e8ed27
al0214 <sisang951@hansei.sen.hs.kr>
2023-05-07 23:25:37 +0900

    03_Rewrite

--- MS_Empowerment_Project/3_week/students.py
+++ MS_Empowerment_Project/3_week/students.py
@@ -26,4 +26,44 @@ def insert_table(name, age, major):
     connection.close()
 
 
-insert_table('john', 21, 'Computer Science')
+# insert_table('john', 21, 'Computer Science')
+
+def query_students():
+    connection = sqlite3.connect('students.db')
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM students")
+    rows = cursor.fetchall()
+
+    connection.close()
+
+    return rows
+
+
+print(query_students())
+
+
+def update_student(student_id, name, age, major):
+    connection = sqlite3.connect('students.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''UPDATE students SET name = ?, age = ?, major = ? WHERE id = ?''',
+                   (name, age, major, student_id))
+
+    connection.commit()
+    connection.close()
+
+
+# update_student(1, "jhon", 22, "Data Science")
+# print(query_students())
+
+def delete_student(student_id):
+    connection = sqlite3.connect('students.db')
+    cursor = connection.cursor()
+
+    cursor.execute("DELETE FROM students WHERE id = ?", (student_id),)
+    connection.commit()
+    connection.close()
+
+    delete_student(1)
+    print(query_students())
