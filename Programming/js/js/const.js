8ae324a2a0623b3db88cebf6e34f7bb656f15849
al0214 <sisang951@hansei.sen.hs.kr>
2023-06-01 19:12:37 +0900

    File_ReBuild

---
+++ Programming/js/js/const.js
@@ -0,0 +1,9 @@
+const number1 = 10;
+const number2 = 20;
+const sum = number1 + number2;
+console.log(sum);
+
+const firstName = "브렌던";
+const familyName = "아이크";
+const fullName = firstName + familyName;
+console.log(fullName);
---
+++ Programming/js/js/let.js
@@ -0,0 +1,13 @@
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+console.log(sum);
+
+let firstName = "브렌던";
+let familyName = "아이크";
+let fullName = firstName + familyName;
+console.log(fullName);
+
+let value1 = 100;
+let value2 = value1;
+console.log(value2);
