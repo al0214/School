bfe155e673e5f8c5b3cc68147f01e238d15f886f
al0214 <sisang951@hansei.sen.hs.kr>
2023-08-27 19:18:03 +0900

    a

---
+++ company/board/src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,52 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+    private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        int saveResult = boardService.save(boardDTO);
+        if (saveResult > 0) {
+            return "redirect:/board/paging";
+        } else {
+            return "save";
+        }
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model){
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ company/board/src/main/java/com/codingrecipe/board/repository/BoardRepository.java
@@ -0,0 +1,22 @@
+package com.codingrecipe.board.repository;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class  BoardRepository {
+    private final SqlSessionTemplate sql;
+    public int save(BoardDTO boardDTO) {
+        return sql.insert("Board.save", boardDTO);
+    }
+
+
+    public List<BoardDTO> findAll() {
+        return sql.selectList("Board.findAll");
+    }
+}
